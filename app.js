0cf76c7d5b838e114d1d16f1334ace4291858b9e
AhmetAKKOC <akkocahmet5335@gmail.com>
2025-08-20 13:09:01 +0300

    mw updates

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const path = 'path';
+const ejs=require('ejs');
+const path = require('path');
 
 const app = express();
+//TEMPLATE ENGINE
+app.set("view engine" , "ejs");
+
+//MIDDLEWARES
 app.use(express.static('public'));
-
+//ROUTES 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index')
 });
-
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
